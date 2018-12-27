0b668485676b51cd980f38550c1f453adcc224b6
Isiah Johnson M. Obera <ijobera@springvalley.tech>
2018-12-27 13:57:23 +0800

    Delete User with Ajax Request and SweetAlert Modal in Laravel and Vuejs

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -34,7 +34,7 @@
                     		<i class="fa fa-edit blue"></i>
                     	</a>
 						/
-                    	<a href="#">
+                    	<a href="#" @click="deleteUser(user.id)">
                     		<i class="fa fa-trash red"></i>
                     	</a>
                     </td>
@@ -132,6 +132,33 @@
 			}
 		},
 		methods: {
+
+			deleteUser(id){
+				swal({
+					title: 'Are you sure?',
+					text: "You won't be able to revert this!",
+					type: 'warning',
+					showCancelButton: true,
+					confirmButtonColor: '#3085d6',
+					cancelButtonColor: '#d33',
+					confirmButtonText: 'Yes, delete it!'
+				}).then((result) => {
+
+					// Send request to the server
+					if(result.value) {
+							this.form.delete('/api/user/'+id).then(() => {
+									swal(
+										'Deleted!',
+										'Your file has been deleted.',
+										'success'
+									)
+									Fire.$emit('AfterCreate');
+							}).catch(() => {
+								swal("Failed!", "There was something wrong.", "warning");
+							});
+					}
+				})
+			},
 
 			loadUsers(){
 				axios.get("api/user").then(({ data }) => (this.users = data.data));
