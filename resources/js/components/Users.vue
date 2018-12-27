28795d96789efe193732d68c66ca2b739cf64b7f
Isiah Johnson M. Obera <ijobera@springvalley.tech>
2018-12-27 15:39:50 +0800

    Conditionally Switch Between Edit Mode and Create Mode of Modal Window

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -53,13 +53,14 @@
 		  <div class="modal-dialog modal-dialog-centered" role="document">
 		    <div class="modal-content">
 		      <div class="modal-header">
-		        <h5 class="modal-title" id="addNewLabel">Add New</h5>
+		        <h5 v-show="!editmode" class="modal-title" id="addNewLabel">Add New</h5>
+						<h5 v-show="editmode" class="modal-title" id="addNewLabel">Update User's Info</h5>
 		        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 		          <span aria-hidden="true">&times;</span>
 		        </button>
 		      </div>
 
-		      <form @submit.prevent="createUser">
+		      <form @submit.prevent="editmode ? updateuser() : createUser()">
 			      <div class="modal-body">
 
 			        <div class="form-group">
@@ -105,7 +106,8 @@
 
 			      <div class="modal-footer">
 			        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-			        <button type="submit" class="btn btn-primary">Create</button>
+			        <button v-show="editmode" type="submit" class="btn btn-success">Update</button>
+							<button v-show="!editmode" type="submit" class="btn btn-primary">Create</button>
 			      </div>
 		      	</form>
 
@@ -120,6 +122,7 @@
 	export default {
 		data() {
 			return {
+				editmode: false,
 				users: {},
 				form: new Form({
 					name: '',
@@ -132,13 +135,19 @@
 			}
 		},
 		methods: {
+			updateUser(){
+				console.log('updating');
+			},
+
 			editModal(user){
+				this.editmode = true;
 				this.form.reset();
 				$('#addNew').modal('show');
 				this.form.fill(user);
 			},
 
 			newModal(){
+				this.editmode = false;
 				this.form.reset();
 				$('#addNew').modal('show');
 			},
