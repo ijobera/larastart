745a837a2b61cedba6012175cd2be70b475e90a6
Isiah Johnson M. Obera <ijobera@springvalley.tech>
2019-01-10 15:59:05 +0800

    Re-Usable Search Functionality in Laravel 5.7 and Vuejs ( Ajax, Axios )

--- public/js/app.js
+++ public/js/app.js
@@ -2349,6 +2349,12 @@ __webpack_require__.r(__webpack_exports__);
   created: function created() {
     var _this6 = this;
 
+    Fire.$on('searching', function () {
+      var query = _this6.$parent.search;
+      axios.get('api/findUser?q=' + query).then(function (data) {
+        _this6.users = data.data;
+      }).catch(function () {});
+    });
     this.loadUsers();
     Fire.$on('AfterCreate', function () {
       _this6.loadUsers();
@@ -77927,7 +77933,15 @@ Vue.component('example-component', __webpack_require__(/*! ./components/ExampleC
 
 var app = new Vue({
   el: '#app',
-  router: router
+  router: router,
+  data: {
+    search: ''
+  },
+  methods: {
+    searchit: function searchit() {
+      Fire.$emit('searching');
+    }
+  }
 });
 
 /***/ }),
